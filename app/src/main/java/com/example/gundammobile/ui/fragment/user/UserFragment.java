8f306e85db1b710e31a0b46e0d803567c1675f0a
Nguyen Anh Duy <aduy1912@gmail.com>
2024-06-13 11:44:59 +0700

    userFragment v0.5

--- app/src/main/java/com/example/gundammobile/ui/fragment/user/UserFragment.java
+++ app/src/main/java/com/example/gundammobile/ui/fragment/user/UserFragment.java
@@ -24,8 +24,8 @@ public class UserFragment extends Fragment {
         binding = FragmentUserBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textUser;
-        userViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textUser;
+//        userViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
